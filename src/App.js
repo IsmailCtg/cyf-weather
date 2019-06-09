33d87641766c36a69d627c54d2144e009e9e8733
Ismail <a.akbas.ismail@gmail.com>
2019-06-09 13:59:40 +0100

    Added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,13 @@ class App extends Component {
     return (
       <div className="app">
         <header className="app__header">
-        </header>
+        <h2>Say Hello to Todays Weather Ismail</h2>
+       </header>
+        <form>
+          <input type="text" name="FirstName" value="Type in a city name"/> 
+          <input type="submit" value="Find Weather" this.Text= string.empty/>
+          
+        </form>
         Ismail
         <main className="app__main">
         </main>
